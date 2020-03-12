0e2bef4e896a8e59039de4fdad2a77a9d9e91c00
Thanh <thanh@ischool.berkeley.edu>
2020-03-12 00:59:27 -0700

    getting closer to app working

--- website/app.py
+++ website/app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
+
+MAPBOX_ACCESS_KEY = 'pk.eyJ1IjoidGxldWNiIiwiYSI6ImNrN2N3d3h1aTA0YWwzaHFoNGJreDJmY2YifQ.pbWSn9txb4n8fKmUaKAG4g'
+
+lat_input = [37.87119088,37.87119088,37.87260911,37.87260911,37.87119088]
+lon_input = [-122.2593983,-122.2576017,-122.2576017,-122.2593983,-122.2593983]
+
+map_output = [[a, b] for a, b in zip(lon_input, lat_input)]
+
+
 @app.route('/')
-
 def index():
-	return render_template('index.html')
+	return render_template('index.html', ACCESS_KEY = MAPBOX_ACCESS_KEY, output = map_output)
 
+# @app.route('/polygon')
+# def polygon_map():
+# 	return 
 
 if __name__ == '__main__':
 	app.run(debug=True)
