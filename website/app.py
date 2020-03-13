8ded37677c3ae50da647236ccfb6670de2b4a407
Thanh <thanh@ischool.berkeley.edu>
2020-03-12 21:16:05 -0700

    update changes to flask app

--- website/app.py
+++ website/app.py
@@ -1,24 +1,33 @@
+import ast
+import csv
+from csv import reader
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
-
+# mapbox api
 MAPBOX_ACCESS_KEY = 'pk.eyJ1IjoidGxldWNiIiwiYSI6ImNrN2N3d3h1aTA0YWwzaHFoNGJreDJmY2YifQ.pbWSn9txb4n8fKmUaKAG4g'
 
-lat_input = [37.87119088,37.87119088,37.87260911,37.87260911,37.87119088]
-lon_input = [-122.2593983,-122.2576017,-122.2576017,-122.2593983,-122.2593983]
+# setting up list
+lat_input = []
+lon_input = []
+
+# read CSV file but skipped header line in read
+with open('static/polygon.csv', 'r') as read_line:
+    csv_reader = reader(read_line)
+    header = next(csv_reader)
+    # Check file as empty
+    if header != None:
+        # Iterate over each row after the header in the csv
+        for row in csv_reader:
+        	lat_input.append(ast.literal_eval(row[1]))
+        	lon_input.append(ast.literal_eval(row[0]))
 
 map_output = [[a, b] for a, b in zip(lon_input, lat_input)]
-
 
 @app.route('/')
 def index():
 	return render_template('index.html', ACCESS_KEY = MAPBOX_ACCESS_KEY, map_output = map_output)
-
-# @app.route('/polygon')
-# def polygon_map():
-# 	return 
 
 if __name__ == '__main__':
 	app.run(debug=True)
