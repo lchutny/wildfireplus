43254d821d7bc7b2166121aaf16014e825ebfa22
Thanh <thanh@ischool.berkeley.edu>
2020-03-15 00:42:22 -0700

    revise webpages codes to be used for variable inputs

--- website/hotzone.py
+++ website/hotzone.py
@@ -1,18 +1,27 @@
 import ast
+import json
 import csv
+import mapbox
+import urllib
+import gmplot
+import requests
 from csv import reader
-from flask import Flask, render_template
+from mapbox import Geocoder
+from flask import Flask, request, render_template
+from math import radians, degrees, sin, cos, asin, acos, sqrt
+# from wtforms import Form
 
 # initialize Flask
 app = Flask(__name__)
 
-# mapbox api
+# mapbox api key
 MAPBOX_ACCESS_KEY = 'pk.eyJ1IjoidGxldWNiIiwiYSI6ImNrN2N3d3h1aTA0YWwzaHFoNGJreDJmY2YifQ.pbWSn9txb4n8fKmUaKAG4g'
 
-
-# lat = {{lat_loc}}
-# print(lat)
-
+# function to calculate distance between two sets of coordinates
+def great_circle(lat1, lon1, lat2, lon2):
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # use 6371 if calculating kilometers (3958.756 for miles)
+    return 3958.756 * (acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2)))
 
 # function to read csv file
 def read_csv():
@@ -40,17 +49,88 @@ def read_csv():
 
 	return map_output
 
+# def lat_loc():
+# 	text = request.form['text']
+#     address = text.upper()
+# 	# address = input("Enter address (example: 2000 Carleton Street, Berkeley, CA): ")
+# 	return address
+
+# function to retrieve latitude and longitude from address entry
+def get_lat_loc(address):
+	geocoder = mapbox.Geocoder(access_token = MAPBOX_ACCESS_KEY)
+	response = geocoder.forward(address)
+	address = response.json()
+	add_lat = address["features"][0]["center"][1]
+	add_lon = address["features"][0]["center"][0]
+	return add_lat, add_lon
+
+# function to find coordinates of active fires of given orgin
+def active_fire(lat_orgin, lon_orgin):
+
+	# read JSON on active fire
+	url = 'https://www.fire.ca.gov/umbraco/api/IncidentApi/List?inactive=true'
+	geo_data = requests.get(url).json()
+
+	# set 
+	lat_geo = []
+	lon_geo = []
+	geo_list = []
+
+	for i in range(0,len(geo_data)):
+	    if geo_data[i]['IsActive'] == "Y":
+	        fire_lat = geo_data[i]['Latitude']
+	        fire_lon = geo_data[i]['Longitude']
+	        dist = great_circle(lat_orgin, lon_orgin, fire_lat, fire_lon)
+	        if dist <= 100:
+	        	lat_geo.append(fire_lat)
+	        	lon_geo.append(fire_lon)
+
+	# geo_list = [[a, b] for a, b in zip(lon_geo, lat_geo)]
+
+	geo_list = [[-122.25760168471588, 37.87119087974465],[-122.25760168471588, 37.87260911343006]]
+	return geo_list
+
 @app.route('/')
 def index():
-	map_output = read_csv()
-	return render_template('index.html', ACCESS_KEY = MAPBOX_ACCESS_KEY, map_output = map_output)
+	return render_template('index.html')
+
+# class ReusableForm(Form):
+#     name = TextField('Address:')
+    
+#     @app.route("/fire_map", methods=['GET', 'POST'])
+#     def hello():
+#         form = ReusableForm(request.form)
+    
+#         print (form.errors)
+#         if request.method == 'POST':
+#             name=request.form['name']
+#             print (name)
+    
+#         # if form.validate():
+#         #     # Save the comment here.
+#         #     flash('Hello ' + name)
+#         # else:
+#         #     flash('All the form fields are required. ')
+#         return render_template('hello.html', form=form)
 
 @app.route('/fire_map')
 def fire_map():
+	geo_list = []
+	add_loc = []
+	add_lat, add_lon = get_lat_loc('2535 channing way, Berkeley, CA')
+	geo_list = active_fire(add_lat, add_lon)
 	map_output = read_csv()
-	# lat, lon = raw_input("Enter your coordinates, in form of latitude, longitude")
-	return render_template('/fire_map.html', ACCESS_KEY = MAPBOX_ACCESS_KEY, map_output = map_output)
+	return render_template('/fire_map.html', 
+							ACCESS_KEY = MAPBOX_ACCESS_KEY, 
+							map_output = map_output, 
+							add_loc = [add_lon, add_lat],
+							geo_list = geo_list)
 
+# @app.route('/fire_map', methods=['POST'])
+# def fire_map_post():
+#     text = request.form['text']
+#     processed_text = text.upper()
+#     return processed_text
 
 if __name__ == '__main__':
 	app.run(debug=True)
