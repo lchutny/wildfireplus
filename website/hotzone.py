6c843f117fe090a358c1ba6e6525e4a7dce005e0
Thanh <thanh@ischool.berkeley.edu>
2020-03-29 19:16:31 -0700

    testing polygon codes

--- website/hotzone.py
+++ website/hotzone.py
@@ -145,49 +145,50 @@ def active_fire(lat_origin, lon_origin):
 	lon_geo = []
 	geo_center = []
 
-	# # read JSON on active fire
-	# url = 'https://www.fire.ca.gov/umbraco/api/IncidentApi/List?inactive=true'
-	# geo_data = requests.get(url).json()
-
-	# # loop through list provided by Cal Fire and find active fire within certain miles of origin
-	# for i in range(0,len(geo_data)):
-	#     if geo_data[i]['IsActive'] == "Y":
-	#         fire_lat = geo_data[i]['Latitude']
-	#         fire_lon = geo_data[i]['Longitude']
-	#         dist = great_circle(lat_origin, lon_origin, fire_lat, fire_lon)
-	#         if dist <= 100:
-	#         	lat_geo.append(fire_lat)
-	#         	lon_geo.append(fire_lon)
-
-	# geo_center = [[a, b] for a, b in zip(lon_geo, lat_geo)]
-
-	# geo_center = [[-121.9230432, 36.52439536]]
-
-
 	# read JSON on active fire
-	url = 'https://opendata.arcgis.com/datasets/5da472c6d27b4b67970acc7b5044c862_0.geojson'
-	geo_data = requests.get(url).json()	
+	url = 'https://www.fire.ca.gov/umbraco/api/IncidentApi/List?inactive=true'
+	geo_data = requests.get(url).json()
 
-	for i in range(0,len(geo_data["features"])):
+	# loop through list provided by Cal Fire and find active fire within certain miles of origin
+	for i in range(0,len(geo_data)):
+	    if geo_data[i]['IsActive'] == "Y":
+	        fire_lat = geo_data[i]['Latitude']
+	        fire_lon = geo_data[i]['Longitude']
+	        dist = great_circle(lat_origin, lon_origin, fire_lat, fire_lon)
+	        if dist <= 100:
+	        	lat_geo.append(fire_lat)
+	        	lon_geo.append(fire_lon)
+
+	geo_center = [[a, b] for a, b in zip(lon_geo, lat_geo)]
+
+	geo_center = [[-121.9230432, 36.52439536]]
+
+
+	# # read JSON on active fire
+	# url = 'https://opendata.arcgis.com/datasets/5da472c6d27b4b67970acc7b5044c862_0.geojson'
+	# geo_data = requests.get(url).json()	
+
+	# for i in range(0,len(geo_data["features"])):
 	    
-	    geo_poly = geo_data["features"][i]["geometry"]["coordinates"][0]
+	#     geo_poly = geo_data["features"][i]["geometry"]["coordinates"][0]
 
-	    for j in range(0, len(geo_poly)):
+	#     for j in range(0, len(geo_poly)):
 
-	    	fire_lon = geo_poly[j][0]
-	    	fire_lat = geo_poly[j][1]
+	#     	fire_lon = geo_poly[j][0]
+	#     	fire_lat = geo_poly[j][1]
 
-	    	dist = great_circle(lat_origin, lon_origin, fire_lat, fire_lon)
+	#     	dist = great_circle(lat_origin, lon_origin, fire_lat, fire_lon)
 
-	    	if dist <= 100:
-	        	geo_center = [fire_lon, fire_lat]
-	        	break
+	#     	if dist <= 100:
+	#         	geo_center = [fire_lon, fire_lat]
+	#         	break
 
-	    if len(geo_center) > 0:
-	        break
+	#     if len(geo_center) > 0:
+	#         break
 
-	return geo_center, geo_poly
+	# return geo_center, geo_poly
 
+	return geo_center
 
 @app.route('/')
 def index():
